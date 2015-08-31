fedbfc49b425e99167389a140b659efe1da795cc
Jannick Fahlbusch <git@jf-projects.de>
2015-08-31 21:01:17 +0200

    Ability to specify the map-type

--- maps.php
+++ maps.php
@@ -5,6 +5,23 @@ class maps extends Script
 {
     public function run()
     {
-		return $this->send($this->message->number, 'http://maps.google.com/maps/api/staticmap?markers=' . rawurlencode($this->matches[1]) . '&size=640x640&maptype=roadmap&sensor=false&format=png', 'image');
+        //map|roadmap|hybridmap|satellitemap
+        print_r($this->matches);
+        switch (strtolower($this->matches[1])) {
+            case 'hybridmap':
+                $this->type = 'hybrid';
+                break;
+            case 'satellitemap':
+                $this->type = 'satellite';
+                break;
+            case 'terrainmap':
+                $this->type = 'terrain';
+                break;
+            default:
+                $this->type = 'roadmap';
+                break;
+        }
+
+		return $this->send($this->message->number, 'http://maps.google.com/maps/api/staticmap?markers=' . rawurlencode($this->matches[2]) . '&size=640x640&maptype=' . $this->type . '&sensor=false&format=png', 'image');
     }
 }
