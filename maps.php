4c77e872ab5448c704d19d48df1eaca390a6f37a
Jannick Fahlbusch <jannickfahlbusch@users.noreply.github.com>
2015-08-31 13:06:35 +0200

    New structure

--- maps.php
+++ maps.php
@@ -1,30 +1,10 @@
 <?php
-/**
- *
- */
-class maps
+require_once 'scripts/script.php';
+
+class maps extends Script
 {
-    //This is holding our matches
-    private $matches;
-
-
-    function __construct($matches)
-    {
-        $this->matches = $matches;
-    }
-
-    public function returnType()
-    {
-        return 'image';
-    }
     public function run()
     {
-
-        return 'http://maps.google.com/maps/api/staticmap?markers=' . rawurlencode($this->matches[1]) . '&size=640x640&maptype=roadmap&sensor=false&format=png';
-    }
-
-    function __destruct()
-    {
-
+		return $this->send($this->message->number, 'http://maps.google.com/maps/api/staticmap?markers=' . rawurlencode($this->matches[1]) . '&size=640x640&maptype=roadmap&sensor=false&format=png', 'image');
     }
 }
